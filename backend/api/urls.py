a04c3b55671e3d3c9d7654012a4bc1331922d05c
fabianolate0320 <f.olate@duocuc.cl>
2025-06-16 01:06:54 -0400

    Se crea la estructura inicial del backend en Django, incluyendo archivos de configuración, entorno y módulos básicos de la API. Se añade la conexión a la base de datos y se implementa un endpoint de prueba.

---
+++ backend/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Aquí irán tus rutas de API
+
+]
---
+++ backend/api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse
+
+
+@api_view(['GET'])
+def hello_world(request):
+    return Response({"message": "Hello, World!"})
+
+
